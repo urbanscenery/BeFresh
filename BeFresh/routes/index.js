a4a09e154338acc277312198fc74add249141e35
YeonTaeKIM <urbanscenery@gmail.com>
2017-06-29 04:26:44 +0900

    커뮤니티 메인화면 완성

--- BeFresh/routes/index.js
+++ BeFresh/routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 var login = require('./login/login');
 var signin = require('./login/signin');
 var membership = require('./membership/index');
+var community = require('./community/index');
 
 router.use('/login', login);
 router.use('/signin', signin);
 router.use('/membership', membership);
+router.use('/community', community);
 
 
 module.exports = router;
