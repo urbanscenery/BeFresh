39691a711fc0ece98097798e980b06a76c2726e4
YeonTaeKIM <urbanscenery@gmail.com>
2017-06-30 02:58:46 +0900

    recipe 만들기 시작, index완료

---
+++ BeFresh/routes/recipe/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var main = require('./main');
+var wellbeingFilter = require('./wellbeingFilter');
+var vegetarianFilter = require('./vegetarianFilter');
+var content = require('./content');
+var card = require('./card');
+var review = require('./review');
+var search = require('./search');
+
+router.use('/main', main);
+router.use('/filter/wellbeing',wellbeingFilter);
+router.use('/filter/vegetarian', vegetarianFilter);
+router.use('/content',content);
+router.use('/content/card', card);
+router.use('/content/review', review);
+router.use('/search', search);
+
+
+module.exports = router;
