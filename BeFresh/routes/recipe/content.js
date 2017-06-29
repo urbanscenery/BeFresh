ae8755b1b28a0f280edd9f4f5e92a0d0484c1242
YeonTaeKIM <urbanscenery@gmail.com>
2017-06-30 06:10:16 +0900

    레시피 상세보기 완료

--- BeFresh/routes/recipe/content.js
+++ BeFresh/routes/recipe/content.js
@@ -9,7 +9,7 @@ const moment = require('moment');
 aws.config.loadFromPath('./config/aws_config.json');
 const pool = require('../../config/db_pool');
 
-router.get('/', function(req, res){
+router.get('/:id', function(req, res){
   let task_array = [
     //1. connection 설정
     function(callback){
@@ -36,6 +36,71 @@ router.get('/', function(req, res){
         else callback(null, decoded.user_email, connection);
       });
     },
+    function(userEmail, connection, callback){
+      let getRecipeQuery = 'select * from recipes where recipe_id = ?';
+      connection.query(getRecipeQuery, req.params.id, function(err, recipeData){
+        if(err){
+          res.status(501).send({
+            msg : "501 get Recipe error"
+          });
+          callback("getRecipeQuery err : "+ err, null);
+        }
+        else{
+          let material_list = JSON.parse(recipeData[0].recipe_material);
+          let method = JSON.parse(recipeData[0].recipe_method);
+          let imageSet = JSON.parse(recipeData[0].recipe_image);
+          let recipe = {
+            id : recipeData[0].recipe_id,
+            imageSet : imageSet,
+            title : recipeData[0].recipe_title,
+            subtitle : recipeData[0].recipe_subtitle,
+            hashtag : recipeData[0].recipe_tag,
+            cookingTime : recipeData[0].recipe_cookingTime,
+            difficulty : recipeData[0].recipe_difficulty,
+            description : recipeData[0].recipe_description,
+            method : method,
+            material_image : recipeData[0].recipe_material_image,
+            material_list : material_list.material,
+            checkSaveList : false
+          };
+          callback(null, recipe, userEmail, connection);
+        }
+      });
+    },
+    function(recipeData, userEmail, connection, callback){
+      let getReviewQuery = 'select * from reviews where recipe_id = ? '+
+      'order by review_post_time '+
+      'limit 2';
+      connection.query(getReviewQuery, req.params.id, function(err, reviewData){
+        if(err){
+          res.status(501).send({
+            msg : "501 get Recipe error"
+          });
+          callback("getRecipeQuery err : "+ err, null);
+        }
+        else{
+          let data_list = [];
+          for(let i = 0; i < reviewData.length; i++){
+            let review = {
+              writer : reviewData[i].user_email,
+              content : reviewData[i].review_content,
+              score : reviewData[i].review_score
+            };
+            data_list.push(review);
+          }
+          callback(null,data_list ,recipeData, connection);
+        }
+      });
+    },
+    function(reviewData, recipeData, connection, callback){
+      res.status(200).send({
+        msg : "Success",
+        data : {
+          recipe : recipeData,
+          review : reviewData
+        }
+      });
+    }
   ];
   async.waterfall(task_array, function(err, result) {
     if (err) console.log(err);
